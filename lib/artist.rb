07d3fac5a4917991b2639240fbdd937ca3034554
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-08-01 22:08:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,19 +19,27 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name)
+<<<<<<< HEAD
     all.detect do |artist| 
     artist.name == name 
     end || new(name) 
+=======
+    all.detect { |artist| artist.name == name }
+    || new(name) 
+>>>>>>> 4f78f3957e5704d68c9f2b8d9bb00fa5500983a9
   end 
   
   def songs
     Song.all.select { |song| song.artist == self }
   end 
   
+<<<<<<< HEAD
   def print_songs
     songs.each do |song|
       puts song.name
     end 
   end 
   
+=======
+>>>>>>> 4f78f3957e5704d68c9f2b8d9bb00fa5500983a9
 end 
